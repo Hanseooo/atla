0a9d6bef6c4687a9f806c6e35899a2fda5549c6c
RANDAAAALL <lesterandig17@gmail.com>
2026-02-09 20:21:27 +0800

    feat: added new error handling and made some changes on ui for temporary.

--- frontend/atla/src/routes/index.tsx
+++ frontend/atla/src/routes/index.tsx
@@ -1,16 +1,27 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { requireAuth } from '../lib/auth-guards'
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { requireAuth } from "../lib/auth-guards";
+import { useAuthStore } from "@/stores/authStore";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: HomePage,
   beforeLoad: requireAuth,
-})
+});
 
 function HomePage() {
+  const navigate = useNavigate()
+  const signOut = useAuthStore((state) => state.signOut)
+  // const isLoading = useAuthStore((state) => state.isLoading)
+
   return (
     <div>
       <div>Home Page - Trip Dashboard</div>
       <div>Welcome! Your trips will appear here.</div>
+      <button 
+      onClick={() => {
+        signOut()
+        navigate({ to: '/login' })
+      }}
+      className="text-white">Signout</button>
     </div>
-  )
+  );
 }
